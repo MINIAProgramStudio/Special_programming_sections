7b27b5452d97eef039024faa7f935108d2e1f57c
Mykhailo Holub <mishataube2005@gmail.com>
2023-10-02 18:58:48 +0300

    URL to .txt download

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,1 +1,8 @@
-pass
+import requests
+
+url= "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?provinceID=1&country=UKR&yearlyTag=Weekly&type=Mean&TagCropland=land&year1=1982&year2=2023"
+vhi_url = requests.get(url)
+out = open('vhi_id_16.txt','wb')
+out.write(vhi_url.content)
+out.close()
+print ("VHI is downloaded...")
