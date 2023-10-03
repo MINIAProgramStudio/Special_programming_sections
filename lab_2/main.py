039a8cdd0fe14206fabaf8005094dd9e9343b46e
Mykhailo Holub <mishataube2005@gmail.com>
2023-10-03 22:02:18 +0300

    done?

--- lab_2/main.py
+++ lab_2/main.py
@@ -34,10 +34,12 @@ def transform_regions_NOAA_to_ua(index):
     elif index == 26: return 7
     elif index == 27: return 5
 
-def download_txt_from_NOAA():
+def download_txt_from_NOAA(log = False):
     url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?provinceID=%s&country=UKR&yearlyTag=Weekly&type=Mean&TagCropland=land&year1=1982&year2=2023"
-    for i in range(1,27):
+    for i in range(1,28):
         downloader.to_txt(url%(i),"downloaded_data/region%s__%s.txt"%(transform_regions_NOAA_to_ua(i), datetime.now().strftime("%Y_%m_%d__%H_%M_%S")))
+        if log:
+            print(">>>downloaded file %s/27"%i)
 
 def clean_directory(path):
     files = os.listdir('downloaded_data')
@@ -48,7 +50,7 @@ def import_txt_to_csv_from_dir(path):
     dataframes = {}
     files = os.listdir(path)
     for file in files:
-        dataframes[file[7:10].replace('_','')] = pd.read_csv(path+"/"+file, skipinitialspace = True, sep=";")
+        dataframes[file[6:9].replace('_','')] = pd.read_csv(path+"/"+file, skipinitialspace = True, sep=";")
     return dataframes
 
 def get_region_VHI(dataframes, region_index):
@@ -59,20 +61,71 @@ def get_year_VHI(dataframes, region_index,year):
     starting_year = int(dataframes[region_index].year.tolist()[0])
     return VHI_list[(year-starting_year)*52:(year-starting_year+1)*52]
 
+def week_index_to_year_week(dataframes,week_index):
+    starting_year = int(dataframes['1'].year.tolist()[0])
+    year = starting_year+int(week_index/52)
+    week = week_index%52
+    return [year, week]
 def find_max_index(in_list):
     return in_list.index(max(in_list))
 def find_min_index(in_list):
     return in_list.index(min(in_list))
 
-def find_max_VHI_yearWeek(dataframes, region_index):
-    pass
+def extreme_drought_area(dataframes, region_index, area_percentage):
+    df = dataframes[region_index]
+    percents_list = df.SMN.tolist()
+    VHI_list = get_region_VHI(dataframes, region_index)
+    i = 0
+    output = []
+    while i < len(VHI_list):
+        if VHI_list[i]>=0 and VHI_list[i]<15 and percents_list[i]>area_percentage/100:
+            year = week_index_to_year_week(dataframes,i)[0]
+            if not year in output:
+                output.append(year)
+        i+=1
+    return output
 
-print(">>>Setup complete")
-#clean_directory("downloaded_data")
-#print(">>>Data directory cleaned")
-#download_txt_from_NOAA()
-#print(">>>Data downloaded")
-dataframes = import_txt_to_csv_from_dir("downloaded_data")
-print(">>>Data imported")
-dataframes['1'].to_csv("1.csv", sep = ';')
-print(">>>Program ended")
+def moderate_drought_area(dataframes, region_index, area_percentage):
+    df = dataframes[region_index]
+    percents_list = df.SMN.tolist()
+    VHI_list = get_region_VHI(dataframes, region_index)
+    i = 0
+    output = []
+    while i < len(VHI_list):
+        if VHI_list[i]>=0 and VHI_list[i]<35 and percents_list[i]>area_percentage/100:
+            year = week_index_to_year_week(dataframes,i)[0]
+            if not year in output:
+                output.append(year)
+        i+=1
+    return output
+
+
+dataframes = {}
+
+def begin():
+    global dataframes
+    print(">>>Setup complete")
+    print(">>>Redownload data? (Y/N)")
+    if input("<<<").lower() in ['y','yes']:
+        clean_directory("Downloaded_data")
+        print(">>>Data deleted")
+        download_txt_from_NOAA(log = True)
+        print(">>>Data downloaded")
+    dataframes = import_txt_to_csv_from_dir("downloaded_data")
+    dataframes['1'].to_csv('1.csv',sep=';')
+
+    print(">>>Data imported")
+
+def main_loop():
+    print(">>>Extreme drought area test")
+    print(extreme_drought_area(dataframes, input("Region<<<"), int(input("Area_percentage<<<"))))
+
+    print(">>>Moderate drought area test")
+    print(moderate_drought_area(dataframes, input("Region<<<"), int(input("Area_percentage<<<"))))
+
+
+
+begin()
+while True:
+    main_loop()
+
