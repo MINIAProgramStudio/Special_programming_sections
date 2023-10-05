66fdd647385772d44473b0692169edf3078afa8d
Mykhailo Holub <mishataube2005@gmail.com>
2023-10-05 23:04:13 +0300

    select_year implemented

--- lab_3/dataframes_dictionary_handler.py
+++ lab_3/dataframes_dictionary_handler.py
@@ -7,3 +7,10 @@ def drom_minus_one_VHI_all(dataframes):
     for k in keys:
         new_dataframes[k] = dataframes[k][dataframes[k]["VHI"] >= 0]
     return new_dataframes
+
+def select_year(dataframes, year):
+    keys = dataframes.keys()
+    new_dataframes = {}
+    for k in keys:
+        new_dataframes[k] = dataframes[k][dataframes[k]["year"] == year]
+    return new_dataframes
