662ebc79ce59f3e4272ce1a46efece54af2946de
Misube <mishataube2005@gmail.com>
2023-12-14 00:23:36 +0200

    lab_5 created

---
+++ lab_5/main.py
@@ -0,0 +1,83 @@
+from random import random
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets import Slider, Button, CheckButtons
+
+fig, ax = plt.subplots(figsize=(10,10))
+#plt.figure()
+plt.subplots_adjust(left=0.25, bottom=0.25)
+x_axis = np.arange(0.0, np.pi*2, 0.001)
+def_opt = {
+    "amplitude" : 1,
+    "frequency" : 1,
+    "phase" : 0,
+    "noise_mean": 0,
+    "noise_disp": 0,
+    "show_noise": False
+}
+
+
+def harmonic_with_noise(amplitude = 1, frequency = 1, phase = 0, noise_mean = 0, noise_disp = 0, show_noise = False):
+    base_line = amplitude * np.sin(frequency*x_axis + phase)
+    if show_noise:
+        done_line = [point+(random()-0.5)*noise_disp+noise_mean for point in base_line]
+        return done_line
+    else:
+        return base_line
+
+l, = plt.plot(x_axis, harmonic_with_noise(), lw=2, color='red')
+plt.axis([0, np.pi*2, -np.pi, np.pi])
+
+ax_color = 'lightgoldenrodyellow'
+ax_freq = plt.axes([0.25, 0.125, 0.65, 0.03], facecolor=ax_color)
+ax_amp = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=ax_color)
+ax_phase = plt.axes([0.25, 0.075, 0.65, 0.03], facecolor=ax_color)
+ax_noise_mean = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=ax_color)
+ax_noise_disp = plt.axes([0.25, 0.025, 0.65, 0.03], facecolor=ax_color)
+
+s_freq = Slider(ax_freq, 'Частота', 0.1, 30.0, valinit=def_opt['frequency'])
+s_amp = Slider(ax_amp, 'Амплітуда', 0.001, np.pi, valinit=def_opt['amplitude'])
+s_phase = Slider(ax_phase, 'Фаза', 0, 2*np.pi, valinit=def_opt['phase'])
+s_noise_mean = Slider(ax_noise_mean, 'N_mean', 0, np.pi, valinit=def_opt['noise_mean'])
+s_noise_disp = Slider(ax_noise_disp, 'N_disp', 0, np.pi, valinit=def_opt['noise_disp'])
+
+
+def update(val):
+    l.set_ydata(harmonic_with_noise(s_amp.val, s_freq.val, s_phase.val, s_noise_mean.val, s_noise_disp.val,def_opt['show_noise']))
+    fig.canvas.draw_idle()
+s_freq.on_changed(update)
+s_amp.on_changed(update)
+s_phase.on_changed(update)
+s_noise_mean.on_changed(update)
+s_noise_disp.on_changed(update)
+
+resetax = plt.axes([0.8, 0, 0.1, 0.04])
+button = Button(resetax, 'Reset', color=ax_color, hovercolor='0.975')
+
+
+def reset(event):
+    s_freq.reset()
+    s_amp.reset()
+    s_phase.reset()
+button.on_clicked(reset)
+
+rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=ax_color)
+radio = CheckButtons(rax, (['Noise']))
+def callback(label):
+    if def_opt['show_noise']:
+        def_opt['show_noise']=False
+    else:
+        def_opt['show_noise'] = True
+    l.set_ydata(harmonic_with_noise(s_amp.val, s_freq.val, s_phase.val, s_noise_mean.val, s_noise_disp.val,
+                                    def_opt['show_noise']))
+    fig.canvas.draw_idle()
+
+radio.on_clicked(callback)
+
+
+
+def colorfunc(label):
+    fig.canvas.draw_idle()
+radio.on_clicked(colorfunc)
+
+plt.show()
