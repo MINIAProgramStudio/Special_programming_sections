5fbb8d2837bc266fdccbd4f5a21727bde70b7b4d
Mykhailo Holub <mishataube2005@gmail.com>
2023-12-28 21:51:37 +0200

    task 2: the begining

--- lab_6/main.py
+++ lab_6/main.py
@@ -41,3 +41,72 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
 plt.show()
+
+#завдання 2
+def regression(x,y,n_iter = 1000, learning_rate = 0.01,b_0 = 0,k_0=1):
+    b_regr = b_0
+    k_regr = k_0
+    for i in range(n_iter):
+        y_i = b_regr + x*k_regr
+        deviation = (y_i/y).mean()
+        if deviation>0.99 and deviation<1.01:
+            break
+        dldb = 0
+        dldk = 0
+        for i in range(len(x)):
+            dldb += y[i]-y_i[i]
+            dldk += x[i]*(y[i] - y_i[i])
+        dldb *=(-2)/len(x)
+        dldk *= (-2) / len(x)
+        b_regr= b_regr - learning_rate * dldb
+        k_regr= k_regr - learning_rate * dldk
+    return [k_regr,b_regr]
+print(regression(x,y))
+
+def regression_plot(x,y,n_iter = 1000, learning_rate = 0.01,b_0 = 0,k_0=1):
+    b_regr = b_0
+    k_regr = k_0
+    plot = []
+    for i in range(n_iter):
+        y_i = b_regr + x*k_regr
+        deviation = (y_i/(y+0.00000000001)).mean()
+        plot.append(deviation)
+        if deviation>0.99 and deviation<1.01:
+            break
+        dldb = 0
+        dldk = 0
+        for i in range(len(x)):
+            dldb += y[i]-y_i[i]
+            dldk += x[i]*(y[i] - y_i[i])
+        dldb *=(-2)/len(x)
+        dldk *= (-2) / len(x)
+        b_regr= b_regr - learning_rate * dldb
+        k_regr= k_regr - learning_rate * dldk
+    return plot
+
+
+#визначення оптимальних параметрів
+learning_rate_best = 0
+iterations_best = 0
+for learning_rate in range(-10,10):
+    if learning_rate <= 0:
+        learning_rate = 10**(learning_rate)
+    plot_array = []
+    for k in range(-10,10):
+        for b in range (-10,10):
+            plot_array.append(regression_plot(x,x*b+k,5000))
+    plot_array_closest = plot_array[:].
+
+
+
+
+#графік точності від кількості ітерацій
+
+
+plt.plot(regression_plot(x,y), color='red')
+plt.xlabel('ітерація')
+plt.ylabel('точність')
+plt.legend()
+plt.show()
+
+#
